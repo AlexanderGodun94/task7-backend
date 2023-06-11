624c99acb925cf16d809af34ed200a8da8aca857
alexander_godun <alexander_godun_1994@mail.ru>
2023-06-11 20:53:47 +0300

    fix algorithm

--- app/core/user/calculateWinnerTicTacToe5x5.js
+++ app/core/user/calculateWinnerTicTacToe5x5.js
@@ -2,10 +2,80 @@ const AppError = require('../../services/error');
 const errorMessages = require('../../services/errorMessages');
 const db = require('../../../app/services/db');
 
+const combinations = [
+  [ 0, 1, 2 ],
+  [ 1, 2, 3 ],
+  [ 2, 3, 4 ],
+
+  [ 5, 6, 7 ],
+  [ 6, 7, 8 ],
+  [ 7, 8, 9 ],
+
+  [ 10, 11, 12 ],
+  [ 11, 12, 13 ],
+  [ 12, 13, 14 ],
+
+  [ 15, 16, 17 ],
+  [ 16, 17, 18 ],
+  [ 17, 18, 19 ],
+
+  [ 20, 21, 22 ],
+  [ 21, 22, 23 ],
+  [ 22, 23, 24 ],
+
+  [ 0, 5, 10 ],
+  [ 5, 10, 15 ],
+  [ 10, 15, 20 ],
+
+  [ 1, 6, 11 ],
+  [ 6, 11, 16 ],
+  [ 11, 16, 21 ],
+
+  [ 2, 7, 12 ],
+  [ 7, 12, 17 ],
+  [ 12, 17, 22 ],
+
+  [ 3, 8, 13 ],
+  [ 8, 13, 18 ],
+  [ 13, 18, 23 ],
+
+  [ 4, 9, 14 ],
+  [ 9, 14, 19 ],
+  [ 14, 19, 24 ],
+
+  [ 0, 6, 12 ],
+  [ 6, 12, 18 ],
+  [ 12, 18, 24 ],
+
+  [ 1, 7, 13 ],
+  [ 7, 13, 19 ],
+
+  [ 2, 8, 14 ],
+
+  [ 5, 11, 17 ],
+  [ 11, 17, 23 ],
+
+  [ 10, 16, 22 ],
+
+  [ 4, 8, 12 ],
+  [ 8, 12, 16 ],
+  [ 12, 16, 20 ],
+
+  [ 3, 7, 11 ],
+  [ 7, 11, 15 ],
+
+  [ 2, 6, 10 ],
+
+  [ 9, 13, 17 ],
+  [ 13, 17, 21 ],
+
+  [ 14, 18, 23 ],
+
+
+];
 
 async function calculateWinner(board) {
   try {
-    const combinations = [];
     let matrix = [];
     for (let i = 0; i < 5; i++) {
       let row = [];
@@ -15,14 +85,6 @@ async function calculateWinner(board) {
       matrix.push(row);
     }
 
-    for (let i = 0; i < matrix.length - 2; i++) {
-      for (let j = 0; j < matrix[i].length - 2; j++) {
-        const horizontal = [matrix[i][j], matrix[i][j + 1], matrix[i][j + 2]];
-        const vertical = [matrix[i][j], matrix[i + 1][j], matrix[i + 2][j]];
-        const diagonal1 = [matrix[i][j], matrix[i + 1][j + 1], matrix[i + 2][j + 2]];
-        const diagonal2 = [matrix[i][j + 2], matrix[i + 1][j + 1], matrix[i + 2][j]];
-        combinations.push(horizontal, vertical, diagonal1, diagonal2);
-      }}
 
     for (let i = 0; i < combinations.length; i++) {
       const [a, b, c] = combinations[i];
